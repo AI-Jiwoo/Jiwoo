01a27f1e08d66bfe444d2bca8646390f864a9423
jeong-gayeon <1bfish106@naver.com>
2024-07-26 20:50:00 +0900

    design/header footer mainLayout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <div className="App">
+import React from 'react';
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import Layout from '../src/component/common/PageLayout';
+import './App.css';
 
-    </div>
-  );
+function App() {
+    return (
+        <ChakraProvider>
+            <Box minHeight="100vh" display="flex" flexDirection="column">
+                <Layout>
+                    <Box flex="1" p={4}>
+                        <h1>Welcome to JIWOO</h1>
+                        {/* 여기에 메인 콘텐츠를 추가하세요 */}
+                    </Box>
+                </Layout>
+            </Box>
+        </ChakraProvider>
+    );
 }
 
 export default App;
